c881cd9acc030d8a678163c9016e1864eb0634c8
PirraToZ <pirratoz@gmail.com>
2024-01-11 03:35:00 +0300

    feat(): add dto

---
+++ app/dto/__init__.py
@@ -0,0 +1,8 @@
+from app.dto.base_dto import BaseModelDto
+from app.dto.user_dto import UserDto
+
+
+__all__ = [
+    "BaseModelDto",
+    "UserDto"
+]
---
+++ app/dto/user_dto.py
@@ -0,0 +1,8 @@
+from pydantic import EmailStr
+
+from app.dto.base_dto import BaseModelDto
+
+
+class UserDto(BaseModelDto):
+    id: int
+    email: EmailStr
