9dc3ecdb465f3d90b9c1dcdec24aa316c239e5bf
PirraToZ <pirratoz@gmail.com>
2024-01-13 03:24:22 +0300

    feat(): add depends auth

--- app/dependencies/__init__.py
+++ app/dependencies/__init__.py
@@ -2,9 +2,11 @@ from app.dependencies.create_session import (
     Session,
     SessionReadOnly,
 )
+from app.dependencies.check_auth import IsAuth
 
 
 __all__ = [
     "Session",
     "SessionReadOnly",
+    "IsAuth",
 ]
---
+++ app/dependencies/check_auth.py
@@ -0,0 +1,52 @@
+from typing import Any
+from abc import ABC
+
+from fastapi import (
+    HTTPException,
+    Depends,
+)
+from fastapi.security import (
+    HTTPAuthorizationCredentials,
+    HTTPBearer,
+)
+from jwt.exceptions import (
+    InvalidTokenError,
+    ExpiredSignatureError,
+)
+
+from app.dependencies.create_session import SessionReadOnly
+from app.repositories.user_repo import UserRepository
+from app.utils import decode_jwt
+
+
+class IsAuth(ABC):
+    security = HTTPBearer()
+
+    async def auth(
+        credentials: HTTPAuthorizationCredentials = Depends(security),
+        session: SessionReadOnly = Depends()
+    ) -> None:
+        payload: dict[str, Any]
+        
+        try:
+            payload = decode_jwt(access_token=credentials.credentials)
+        except ExpiredSignatureError:
+            raise HTTPException(
+                status_code=401,
+                detail="Expired signature"
+            )
+        except InvalidTokenError:
+            raise HTTPException(
+                status_code=401,
+                detail="Token is invalid"
+            )
+
+        id = payload.get("id")
+        
+        user_repo = UserRepository(session)
+
+        if await user_repo.get_user_by_id(id) is None:
+            raise HTTPException(
+                status_code=401,
+                detail="Unauthorized"
+            )
